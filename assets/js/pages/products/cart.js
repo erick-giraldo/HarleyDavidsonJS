8f7ebc5dafad9eb66cfb02918541866c9ee9f38c
Erick Giraldo <egiraldom@outlook.com>
2022-11-11 02:16:55 -0500

    AVANCE 8

--- assets/js/pages/products/cart.js
+++ assets/js/pages/products/cart.js
@@ -1,13 +1,67 @@
 let allContainerCart = document.querySelector(".productos-contenido");
-
 let containerBuyCart = document.querySelector(".card-items");
 let priceTotal = document.querySelector(".price-total");
 let amountProduct = document.querySelector(".count-product");
 let checkOut = document.querySelector(".order-button");
-
+let emptyCarClass = document.querySelector(".empty-button");
+let yape = document.querySelector(".yape");
+let plin = document.querySelector(".plin");
+let form = document.querySelector(".paying-card");
+let selectPayment = document.querySelector(".form-select");
+let btnPayment = document.querySelector(".btn-pay-checkout");
+let infoPayment = document.querySelector(".info-payment");
+let btnPayTxt = document.querySelector(".btn-pay-txt");
+let modalBackdrop = document.querySelector(".modal-backdrop");
+console.log("🚀 ~ file: cart.js ~ line 16 ~ modalBackdrop", modalBackdrop)
 let buyThings = [];
 let totalCard = 0;
-let countProduct = 0;
+
+selectPayment.addEventListener("change", function(){
+  const selectValue = selectPayment.value;
+  btnPayment.style.display = 'flex'
+  infoPayment.style.display = 'block'
+  switch (selectValue) {
+  case "Plin":
+    yape.style.display = 'none'
+    form.style.display = 'none'
+    plin.style.display = 'block'
+    
+    break;
+  case "Yape":
+    plin.style.display = 'none'
+    form.style.display = 'none'
+    yape.style.display = 'block'
+    break
+  case "Tarjeta":
+    plin.style.display = 'none'
+    yape.style.display = 'none'
+    infoPayment.style.display = 'none'
+    form.style.display = 'block'
+    break
+  default:
+    break;
+}
+
+});
+function closeBtn() {
+  document.getElementById("products-id").style.right = "-500px";
+}
+
+btnPayment.addEventListener("click", function(){
+  setTimeout(function () {
+    Swal.fire({
+      icon: "success",
+      title: "Bien Hecho!!",
+      text: "El pago fue validado correctamente",
+    });
+    closeBtn();
+    emptyCar();
+    btnPayment.modal('hide')
+    modalBackdrop.addEventListener("change", function(){
+      // modalBackdrop.style.focus = 0; pendiente
+    })
+  }, 2000);
+});
 
 const Toast = Swal.mixin({
   toast: true,
@@ -35,12 +89,9 @@ const addProduct = (e) => {
 const deleteProduct = (e) => {
   if (e.target.classList.contains("delete-product") || "") {
     const deleteId = e.target.getAttribute("data-id");
-
     buyThings = buyThings.filter((product) => product.id !== deleteId);
     localStorage.setItem("buyThings", JSON.stringify(buyThings));
     loadHtml();
-    // countProduct--;
-    // countProduct <= 0 && (checkOut.style.display = "none");
   }
   if (buyThings.length === 0) {
     priceTotal.innerHTML = 0;
@@ -62,11 +113,9 @@ const readTheContent = (product) => {
     id: product.querySelector(".card-body button").getAttribute("data-id"),
     amount: 1,
   };
-
   totalCard =
     parseFloat(totalCard) + parseFloat(infoProduct.price.replace(/['$]+/g, ""));
   totalCard = totalCard.toFixed(2);
-
   const exist = buyThings.some((product) => product.id === infoProduct.id);
   if (exist) {
     Toast.fire({
@@ -76,14 +125,12 @@ const readTheContent = (product) => {
   } else {
     buyThings = [...buyThings, infoProduct];
     localStorage.setItem("buyThings", JSON.stringify(buyThings));
-    countProduct++;
     loadHtml();
   }
 };
 
 const addAmountitems = (id, type) => {
   buyThings = JSON.parse(localStorage.getItem("buyThings") || []);
-
   buyThings = buyThings.map((product) => {
     if (product.id !== id) return product;
     return {
@@ -103,6 +150,17 @@ const addAmountitems = (id, type) => {
 const clearHtml = () => {
   containerBuyCart.innerHTML = "";
 };
+
+const emptyCar = () => {
+  localStorage.removeItem("buyThings");
+  localStorage.setItem("buyThings", JSON.stringify([]));
+  loadHtml();
+  if (buyThings.length === 0) {
+    priceTotal.innerHTML = 0;
+    amountProduct.innerHTML = 0;
+  }
+};
+const isEmty = emptyCarClass.addEventListener("click", emptyCar);
 
 const loadHtml = () => {
   clearHtml();
@@ -128,10 +186,10 @@ const loadHtml = () => {
            </div>
               </div>
             <span class="delete-product" data-id="${id}">X</span>
+            
         `;
     containerBuyCart.appendChild(row);
     priceTotal.innerHTML = totalCard;
-    amountProduct.innerHTML = countProduct;
   });
 
   const getItemsCard = document.querySelectorAll(".item");
@@ -150,12 +208,14 @@ const loadHtml = () => {
   buyThings.length === 0
     ? (checkOut.style.display = "none")
     : (checkOut.style.display = "block");
+  buyThings.length === 0
+    ? (emptyCarClass.style.display = "none")
+    : (emptyCarClass.style.display = "block");
   let price = 0;
   buyThings.forEach((value) => {
     let priceReduce =
       parseFloat(value.price.replace(/['$]+/g, "")) * parseFloat(value.amount);
     price = Number(price) + Number(priceReduce);
-    // amountProduct.innerHTML = value.amount;
   });
   amountProduct.innerHTML = buyThings.length;
   priceTotal.innerHTML = price.toFixed(2);
--- assets/js/services/api.js
+++ assets/js/services/api.js
@@ -1,22 +1,26 @@
-
-export const getUsersAPi = async () => {
-    try {
-      const response = await fetch(
-        `https://api-harley-davidson.herokuapp.com/users`
-      );
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      dataUsers.push(data);
-    //   insertUsersDom(data);
-    } catch (error) {
-      console.log(error);
-      Swal.fire({
-        icon: "error",
-        showConfirmButton: false,
-        title: "Oops... no se pudo obtener data de usuarios",
-        text: `${error}`,
-      });
+export const getProducts = async () => {
+  try {
+    
+    const response = await fetch(
+      `https://api-harley-davidson.herokuapp.com/modelos`
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  };
+    spinnerMain.style.display = 'flex'
+    const result = await response.json();
+    setTimeout(function(){
+      test(result)
+      spinnerMain.style.display = 'none'
+  },5000);
+  } catch (error) {
+    console.log(error);
+    Swal.fire({
+      icon: "error",
+      showConfirmButton: false,
+      title: "Oops... no se pudo obtener data de usuarios",
+      text: `${error}`,
+    });
+    spinnerMain.style.display = 'none'
+  }
+};
